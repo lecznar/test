b0ad64663f145fbb61a5729b27968af3de10e7c8
llecznar <lecznar@gmx.de>
2023-05-26 11:43:32 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+// let napis1 = 'A';
+// let napis2 = 'k';
+// let napis3 = 'a';
+// let napis4 = 'd';
+// let napis5 = 'e';
+// let napis6 = 'm';
+// let napis7 = 'i';
+// let napis8 = 'a';
+// let napis9 = '108';
+
+// function napis() {
+//     console.log(napis1+napis2+napis3+napis4+napis5+napis6+napis7+napis8+napis9);;    
+// }
+
+// napis();
+
+// napis.reverse();
+
+// console.log(napis);
+
+
+let napis = ['A', 'k', 'a', 'd', 'e', 'm', 'i', 'a', '108'];
+
+console.log(napis.join(''));
+
+napis.reverse();
+console.log(napis.join(''));
